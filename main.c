21dec0a6e196219cef4e0b295cd964dae4a479c2
Ayoub Aarab <ayoubaarab571@gmail.com>
2025-04-15 17:50:04 +0100

    0.00%

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aychikhi <aychikhi@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/10 12:09:50 by aychikhi          #+#    #+#             */
-/*   Updated: 2025/04/10 12:09:51 by aychikhi         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <unistd.h>
+#include <stddef.h>
+#include <sys/wait.h>
 
+int	main(int ac , char **args , char **envp)
+{
+	int pid = fork();
+	printf("%d",pid);
+	if (pid > 0)
+	{
+		execve("/bin/ls", args, envp);
+		printf("%d\n",pid);
+	}
+	else
+	{
+		printf("%d",pid);
+		wait(NULL);
+	}
+}
