136707d4b649c41ab24daa8ef53b7ff6992e79ce
4y0u8 <ayoubaarab571@gmail.com>
2025-04-27 16:16:54 +0100

    Refactor ft_echo function and main function

--- execution/main.c
+++ execution/main.c
@@ -6,36 +6,36 @@
 /*   By: ayaarab <ayaarab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 15:37:47 by ayaarab           #+#    #+#             */
-/*   Updated: 2025/04/27 15:59:24 by ayaarab          ###   ########.fr       */
+/*   Updated: 2025/04/27 16:11:20 by ayaarab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
-int g_exit_status;
+int	g_exit_status;
 
-int main(int argc, char **argv, char **env)
+int	main(int argc, char **argv, char **env)
 {
-    char *line;
+	char	*line;
 
-    (void)argc;
-    (void)argv;
+	(void)argc;
+	(void)argv;
 	g_exit_status = 0;
-    while (1)
-    {
-        line = readline("minishell$ ");
-        if (!line)
-        {
-            printf("exit\n");
-            break;
-        }
-        if (*line)
-        {
-            add_history(line);
-            helper_builtins(line, env);
-        }
-        free(line);
-    }
-    clear_history();
-    return (g_exit_status);
+	while (1)
+	{
+		line = readline("minishell$ ");
+		if (!line)
+		{
+			printf("exit\n");
+			break ;
+		}
+		if (*line)
+		{
+			add_history(line);
+			helper_builtins(line, env);
+		}
+		free(line);
+	}
+	clear_history();
+	return (g_exit_status);
 }
