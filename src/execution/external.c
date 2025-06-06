f2caca885269d78a9d2038ce9a2cdb31ea1a0e1c
4y0u8 <ayoubaarab571@gmail.com>
2025-06-06 22:19:04 +0100

    goof

--- src/execution/external.c
+++ src/execution/external.c
@@ -6,7 +6,7 @@
 /*   By: ayaarab <ayaarab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 18:39:05 by ayaarab           #+#    #+#             */
-/*   Updated: 2025/06/06 21:44:11 by ayaarab          ###   ########.fr       */
+/*   Updated: 2025/06/06 22:14:17 by ayaarab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,88 +14,89 @@
 
 static void	handle_heredoc(t_file *f)
 {
-    if (f->h_fd != -1)
-    {
-        dup2(f->h_fd, STDIN_FILENO);
-        close(f->h_fd);
-    }
-    else
-        ft_putstr_fd("error in the heredoc\n", 2);
+	if (f->h_fd != -1)
+	{
+		dup2(f->h_fd, STDIN_FILENO);
+		close(f->h_fd);
+	}
+	else
+		ft_putstr_fd("error in the heredoc\n", 2);
 }
 
 static void	handle_redir(t_file *f)
 {
-    int	fd;
+	int	fd;
 
-    if (f->type == TOKEN_REDIR_IN)
-        fd = open(f->name, O_RDONLY);
-    else if (f->type == TOKEN_REDIR_OUT)
-        fd = open(f->name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    else if (f->type == TOKEN_APPEND)
-        fd = open(f->name, O_WRONLY | O_CREAT | O_APPEND, 0644);
-    else if (f->type == TOKEN_HEREDOC)
-    {
+	fd = -1;
+	if (f->type == TOKEN_REDIR_IN)
+		fd = open(f->name, O_RDONLY);
+	else if (f->type == TOKEN_REDIR_OUT)
+		fd = open(f->name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else if (f->type == TOKEN_APPEND)
+		fd = open(f->name, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	else if (f->type == TOKEN_HEREDOC)
+	{
 		handle_heredoc(f);
-        return;
-    }
-    if (fd < 0)
-        ft_putstr_fd("Error opening file: ", 2);
-    else
-    {
-        if (f->type == TOKEN_REDIR_IN)
-            dup2(fd, STDIN_FILENO);
-        else
-            dup2(fd, STDOUT_FILENO);
-        close(fd);
-    }
+		return ;
+	}
+	if (fd < 0)
+		ft_putstr_fd("Error opening file: ", 2);
+	else
+	{
+		if (f->type == TOKEN_REDIR_IN)
+			dup2(fd, STDIN_FILENO);
+		else
+			dup2(fd, STDOUT_FILENO);
+		close(fd);
+	}
 }
 
 void	apply_redirection(t_cmd *cmd)
 {
-    t_file	*f;
+	t_file	*f;
 
-    f = cmd->file;
-    while (f)
-    {
-        handle_redir(f);
-        f = f->next;
-    }
+	f = cmd->file;
+	while (f)
+	{
+		handle_redir(f);
+		f = f->next;
+	}
 }
 
 static void	execute_child_process(t_cmd *cmd, char *path, char **envp)
 {
-    apply_redirection(cmd);
-    set_signals_in_child();
-    execve(path, cmd->args, envp);
-    ft_putstr_fd("minishell: execve failed: ", 2);
-    exit(1);
+	apply_redirection(cmd);
+	set_signals_in_child();
+	execve(path, cmd->args, envp);
+	ft_putstr_fd("minishell: execve failed: ", 2);
+	exit(1);
 }
 
 void	exec_externals(t_cmd *cmd, t_env *env)
 {
-    pid_t	pid;
-    char	*path;
-    char	**envp;
-    int		status;
+	pid_t	pid;
+	char	*path;
+	char	**envp;
+	int		status;
 
-    path = get_cmd_path(cmd->cmd, env);
-    if (!path)
-    {
-        ft_putstr_fd("minishell: command not found", 2);
-        ft_putstr_fd(cmd->cmd, 2);
-        ft_putstr_fd("\n", 2);
-        g_exit_status = 127;
-        return ;
-    }
-    envp = list_to_env(env);
-    pid = fork();
-    if (pid == 0)
-        execute_child_process(cmd, path, envp);
-    else
-    {
-        waitpid(pid, &status, 0);
-        update_exit_status(status);
-    }
-    free(path);
-    free_2d_arr(envp);
+	path = get_cmd_path(cmd->cmd, env);
+	if (!path)
+	{
+		ft_putstr_fd("minishell: command not found", 2);
+		ft_putstr_fd(cmd->cmd, 2);
+		ft_putstr_fd("\n", 2);
+		g_exit_status = 127;
+		return ;
+	}
+	envp = list_to_env(env);
+	pid = fork();
+	if (pid == 0)
+		execute_child_process(cmd, path, envp);
+	else
+	{
+		waitpid(pid, &status, 0);
+		update_exit_status(status);
+	}
+	free(path);
+	free_2d_arr(envp);
 }
