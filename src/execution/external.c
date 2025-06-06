96faef1456d2713cd44d9241feaa38d76c9cb6ca
ayoub <ayoubaarab571@gmail.com>
2025-06-06 22:12:02 +0100

    Refactor redirection handling to separate heredoc logic and improve code clarity

--- src/execution/external.c
+++ src/execution/external.c
@@ -12,11 +12,21 @@
 
 #include "../../includes/minishell.h"
 
-static void	handle_file_redirection(t_file *f)
+static void	handle_heredoc(t_file *f)
+{
+    if (f->h_fd != -1)
+    {
+        dup2(f->h_fd, STDIN_FILENO);
+        close(f->h_fd);
+    }
+    else
+        ft_putstr_fd("error in the heredoc\n", 2);
+}
+
+static void	handle_redir(t_file *f)
 {
     int	fd;
 
-    fd = -1;
     if (f->type == TOKEN_REDIR_IN)
         fd = open(f->name, O_RDONLY);
     else if (f->type == TOKEN_REDIR_OUT)
@@ -25,34 +35,31 @@ static void	handle_file_redirection(t_file *f)
         fd = open(f->name, O_WRONLY | O_CREAT | O_APPEND, 0644);
     else if (f->type == TOKEN_HEREDOC)
     {
-        if (f->h_fd != -1)
-        {
-            dup2(f->h_fd, STDIN_FILENO);
-            close(f->h_fd);
-            return;
-        }
-        else
-            ft_putstr_fd("error in the heredoc\n", 2);
+		handle_heredoc(f);
+        return;
     }
     if (fd < 0)
         ft_putstr_fd("Error opening file: ", 2);
     else
     {
-        dup2(fd, (f->type == TOKEN_REDIR_IN) ? STDIN_FILENO : STDOUT_FILENO);
+        if (f->type == TOKEN_REDIR_IN)
+            dup2(fd, STDIN_FILENO);
+        else
+            dup2(fd, STDOUT_FILENO);
         close(fd);
     }
 }
 
 void	apply_redirection(t_cmd *cmd)
 {
-	t_file	*f;
+    t_file	*f;
 
-	f = cmd->file;
-	while (f)
-	{
-		handle_file_redirection(f);
-		f = f->next;
-	}
+    f = cmd->file;
+    while (f)
+    {
+        handle_redir(f);
+        f = f->next;
+    }
 }
 
 static void	execute_child_process(t_cmd *cmd, char *path, char **envp)
@@ -66,29 +73,29 @@ static void	execute_child_process(t_cmd *cmd, char *path, char **envp)
 
 void	exec_externals(t_cmd *cmd, t_env *env)
 {
-	pid_t	pid;
-	char	*path;
-	char	**envp;
-	int		status;
+    pid_t	pid;
+    char	*path;
+    char	**envp;
+    int		status;
 
-	path = get_cmd_path(cmd->cmd, env);
-	if (!path)
-	{
-		ft_putstr_fd("minishell: command not found", 2);
-		ft_putstr_fd(cmd->cmd, 2);
-		ft_putstr_fd("\n", 2);
-		g_exit_status = 127;
-		return ;
-	}
-	envp = list_to_env(env);
-	pid = fork();
-	if (pid == 0)
-		execute_child_process(cmd, path, envp);
-	else
-	{
-		waitpid(pid, &status, 0);
-		update_exit_status(status);
-	}
-	free(path);
-	free_2d_arr(envp);
+    path = get_cmd_path(cmd->cmd, env);
+    if (!path)
+    {
+        ft_putstr_fd("minishell: command not found", 2);
+        ft_putstr_fd(cmd->cmd, 2);
+        ft_putstr_fd("\n", 2);
+        g_exit_status = 127;
+        return ;
+    }
+    envp = list_to_env(env);
+    pid = fork();
+    if (pid == 0)
+        execute_child_process(cmd, path, envp);
+    else
+    {
+        waitpid(pid, &status, 0);
+        update_exit_status(status);
+    }
+    free(path);
+    free_2d_arr(envp);
 }
